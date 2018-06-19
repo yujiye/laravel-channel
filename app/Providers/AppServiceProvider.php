bd67a8d6c7a652bcdb612ca318d80a8943f9f865
yanyong <512511253@qq.com>
2018-06-19 16:08:16 +0800

    hello channel

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+		// 设置错误级别
+		error_reporting(E_ERROR &~ E_NOTICE);
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
